cefe6dd004577737fdfb3764c842005d20a0ba8e
NBICT LAB <73274402+NBICTLAB@users.noreply.github.com>
2024-02-12 16:47:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Data Analytics", "Data Science", "Machine Learning", "Web Development", "Graphic Design"],
+        strings: ["Data Analytics", "Data Science", "Machine Learning", "Python for Data Science", "R Programming", "SPSS"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
